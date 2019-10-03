ee15f669b3e14bbfdf0b17f52e1f78005085cf5d
farukobradovic <faruk_95@msn.com>
2019-10-03 22:05:14 +0200

    Updates

---
+++ precache-manifest.18812e836d3303188ee13274e5930098.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1dd8fd2df49a073d5fb38c82b065846",
+    "url": "/vadooapp/index.html"
+  },
+  {
+    "revision": "2102de20731a88570129",
+    "url": "/vadooapp/static/css/main.e21475ea.chunk.css"
+  },
+  {
+    "revision": "3b7fb7dc8deb87b9460d",
+    "url": "/vadooapp/static/js/2.95643f9c.chunk.js"
+  },
+  {
+    "revision": "2102de20731a88570129",
+    "url": "/vadooapp/static/js/main.0bbba7ec.chunk.js"
+  },
+  {
+    "revision": "6918abb459bec134dcad",
+    "url": "/vadooapp/static/js/runtime~main.83215a63.js"
+  },
+  {
+    "revision": "9f989783903267aef27a494cb8d9c473",
+    "url": "/vadooapp/static/media/bg-main.9f989783.jpg"
+  }
+]);
